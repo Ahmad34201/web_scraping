56bcbf26fdb7b3393fc79d4ff4788317274d8048
Ahmed Ali <--edit>
2023-07-04 15:43:23 +0500

    Changes covered and converted to OOP

--- polynomial.py
+++ polynomial.py
@@ -1,26 +1,38 @@
 
-# Calculating the Power by using modulo operator
-def fast_power(base, power):
-    power_result = 1
-    MODULE_PRIME = 831121
-    while (power > 0):
-        if ((power % 2) != 0):
-            power_result = (power_result * base % MODULE_PRIME) % MODULE_PRIME
+class Polynomial:
 
-        base = ((base % MODULE_PRIME) * (base % MODULE_PRIME)) % MODULE_PRIME
-        power = power >> 1
-    return power_result
+    def __init__(self, s):
+        self.s = s
 
-# Function to get result for string polynomial
-def get_polynomial(string_to_convert, multiplier_x):
-    polynomial_result = 0
-    MODULE_PRIME = 831121    
+    # Calculating the Power by using modulo operator
+    def fast_power(self, base, power, module_prime):
+        power_result = 1
+        while (power > 0):
+            if ((power % 2) != 0):
+                power_result = (power_result * base %
+                                module_prime) % module_prime
 
-    for power_of_x, character in enumerate(list(string_to_convert[::-1])):
-        polynomial_result = ((polynomial_result % 831121) +
-                             (ord(character)*(fast_power(multiplier_x, power_of_x))) % MODULE_PRIME) % MODULE_PRIME
-    print("Final output is ", polynomial_result)
-    return polynomial_result
+            base = ((base % module_prime) * (base %
+                    module_prime)) % module_prime
+            power = power >> 1
+        return power_result
+
+    # Function to get result for string polynomial
+
+    def get_polynomial(self, string_to_convert, multiplier_x, module_prime):
+        polynomial_result = 0
+
+        for power_of_x, character in enumerate(list(reversed(string_to_convert))):
+            polynomial_result = ((polynomial_result % module_prime) +
+                                 (ord(character)*(self.fast_power(multiplier_x, power_of_x, module_prime))) % module_prime) % module_prime
+
+        return polynomial_result
+
+    def solve(self, x, prime):
+        polynomial = self.get_polynomial(self.s, x, prime)
+        print("Final output is ", polynomial)
 
 
-get_polynomial("hello", 42098)
+MODULE_PRIME = int(input("Enter a prime number\n"))
+polynomial = Polynomial('hello')
+polynomial.solve(42098, MODULE_PRIME)
