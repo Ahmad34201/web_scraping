4142b535d4a5c07c02148df7b0c8f29d6689b8ab
Ahmed Ali <--edit>
2023-07-04 18:44:41 +0500

    Added coefficients list

--- polynomial.py
+++ polynomial.py
@@ -1,8 +1,13 @@
 
 class Polynomial:
 
-    def __init__(self, s):
-        self.s = s
+    def __init__(self, c):
+        self.coefficients = c
+
+    def extract_coefficients(self, polynomial_string):
+        for character in reversed(polynomial_string):
+            self.coefficients.append(ord(character))
+        return self.coefficients
 
     # Calculating the Power by using modulo operator
     def fast_power(self, base, power, module_prime):
@@ -19,20 +24,22 @@ class Polynomial:
 
     # Function to get result for string polynomial
 
-    def get_polynomial(self, string_to_convert, multiplier_x, module_prime):
+    def get_polynomial(self, multiplier_x, module_prime):
         polynomial_result = 0
 
-        for power_of_x, character in enumerate(list(reversed(string_to_convert))):
+        for power_of_x, character in enumerate(self.coefficients):
             polynomial_result = ((polynomial_result % module_prime) +
-                                 (ord(character)*(self.fast_power(multiplier_x, power_of_x, module_prime))) % module_prime) % module_prime
+                                 (character*(self.fast_power(multiplier_x, power_of_x, module_prime))) % module_prime) % module_prime
 
         return polynomial_result
 
     def solve(self, x, prime):
-        polynomial = self.get_polynomial(self.s, x, prime)
+        polynomial = self.get_polynomial(x, prime)
         print("Final output is ", polynomial)
 
 
 MODULE_PRIME = int(input("Enter a prime number\n"))
-polynomial = Polynomial('hello')
+polynomial = Polynomial([111, 108, 108, 101, 104])
 polynomial.solve(42098, MODULE_PRIME)
+polynomial.solve(30098, MODULE_PRIME)
+
